0a01323bc7be117618eb8c48b1b3eac8ad83a8d1
Amaan ansari <amaan.ansari2080@gmail.com>
2025-01-15 19:55:21 +0530

    astrik removed

--- src/modules/Auth/pages/Login.jsx
+++ src/modules/Auth/pages/Login.jsx
@@ -63,6 +63,7 @@ const Login = () => {
               <Form
                 form={form} // Connect form instance directly here
                 name="loginForm"
+                requiredMark={false}
                 layout="vertical"
                 onFinish={onFinish}
                 onFinishFailed={onFinishFailed}
